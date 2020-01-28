59d14786c372a28847a222cbb3f373e071ac0174
haylee tk <haylee430@gmail.com>
2020-01-27 21:52:22 -0600

    done

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
+# Heroku Link - using jawsDB
+
 # CRUD_example
-A website that clearly shows CRUD being used
+A website that clearly shows CRUD comand happening on on-click events
+
+SQL and Sequilize was used as a database platform. 
+
+
+
--- server.js
+++ server.js
@@ -1,23 +1,28 @@
-// modules needed to create functioning application 
 var express = require("express");
-var handlebars = require("express-handlebars")
-var routes = require("./controller/book-controller")
 
-// seeting up varible app and the port - process.env.PORT needed for Heroku
-var app = express();
 var PORT = process.env.PORT || 8080;
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.engine("handlebars", handlebars({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+var app = express();
 
-// Static directory
+// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-app.use("/", routes);
- 
-app.listen(PORT, function () {
-  console.log("App listening on PORT: " + PORT);
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controller/book-controller");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
 });
